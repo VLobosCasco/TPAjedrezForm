06ed689a8afdf93d4eac6e17f013a3e5544cc6ac
Lucio Rodriguez <luciorodriguez10@yahoo.com.ar>
2021-10-29 16:52:23 -0300

    Agregamos la funcion para que muestre el tablero de casillas fatales a la derecha

--- Chess/Chess.Designer.cs
+++ Chess/Chess.Designer.cs
@@ -30,24 +30,51 @@ namespace Chess
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.btn_fatales = new System.Windows.Forms.Button();
+            this.btn_proximoTablero = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Georgia", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(640, 23);
+            this.label1.Location = new System.Drawing.Point(13, 9);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(148, 31);
+            this.label1.Size = new System.Drawing.Size(221, 47);
             this.label1.TabIndex = 0;
             this.label1.Text = "Tablero n° ";
             // 
+            // btn_fatales
+            // 
+            this.btn_fatales.Font = new System.Drawing.Font("Georgia", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_fatales.Location = new System.Drawing.Point(69, 600);
+            this.btn_fatales.Name = "btn_fatales";
+            this.btn_fatales.Size = new System.Drawing.Size(231, 80);
+            this.btn_fatales.TabIndex = 1;
+            this.btn_fatales.Text = "Filtrar casillas fatales";
+            this.btn_fatales.UseVisualStyleBackColor = true;
+            this.btn_fatales.Click += new System.EventHandler(this.btn_fatales_Click);
+            // 
+            // btn_proximoTablero
+            // 
+            this.btn_proximoTablero.Font = new System.Drawing.Font("Georgia", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_proximoTablero.Location = new System.Drawing.Point(317, 600);
+            this.btn_proximoTablero.Name = "btn_proximoTablero";
+            this.btn_proximoTablero.Size = new System.Drawing.Size(238, 80);
+            this.btn_proximoTablero.TabIndex = 2;
+            this.btn_proximoTablero.Text = "Proximo Tablero";
+            this.btn_proximoTablero.UseVisualStyleBackColor = true;
+            // 
             // Chess
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1200, 692);
+            this.Controls.Add(this.btn_proximoTablero);
+            this.Controls.Add(this.btn_fatales);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Chess";
             this.Text = "Chess";
             this.Load += new System.EventHandler(this.Chess_Load);
@@ -59,5 +86,7 @@ namespace Chess
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_fatales;
+        private System.Windows.Forms.Button btn_proximoTablero;
     }
 }
--- Chess/Chess.cs
+++ Chess/Chess.cs
@@ -17,6 +17,9 @@ namespace Chess
     public partial class Chess : Form
     {
         private Panel[,] _chessBoardPanels;
+        public const int tileSize = 40;
+        public const int gridSize = 8;
+        
         public Chess()
         {
             InitializeComponent();
@@ -24,8 +27,7 @@ namespace Chess
 
         private void Chess_Load(object sender, EventArgs e)
         {
-            const int tileSize = 40;
-            const int gridSize = 8;
+            //------------------------Tablero casillas leves-----------------------
             var clr1 = Color.Black;
             var clr2 = Color.White;
 
@@ -42,17 +44,14 @@ namespace Chess
                     var newPanel = new Panel
                     {
                         Size = new Size(tileSize, tileSize),
-                        Location = new Point(tileSize * n, tileSize * m)
+                        Location = new Point(tileSize * n +50, tileSize * m + 50)
                     };
                     
                     // add to Form's Controls so that they show up
                     Controls.Add(newPanel);
 
-                   
-
                     // add to our 2d array of panels for future use
                     _chessBoardPanels[n, m] = newPanel;
-
                     // color the backgrounds
                     if (n % 2 != 0)
                         newPanel.BackColor = m % 2 != 0 ? clr1 : clr2;
@@ -61,11 +60,49 @@ namespace Chess
                 }
             }
             //usamos las piezas blancas pq tienen contorno negro y se ven
-
-            string path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"Assets\White_King.png");
-            _chessBoardPanels[4,3].BackgroundImage = Image.FromFile(path);
-            _chessBoardPanels[4, 3].BackgroundImageLayout = ImageLayout.Stretch;
+            string path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"Assets\White_Rook.png");
+            _chessBoardPanels[0,0].BackgroundImage = Image.FromFile(path);
+            _chessBoardPanels[0, 0].BackgroundImageLayout = ImageLayout.Stretch;
+            _chessBoardPanels[7, 7].BackgroundImage = Image.FromFile(path);
+            _chessBoardPanels[7, 7].BackgroundImageLayout = ImageLayout.Stretch;     
         }
-      
+
+        private void btn_fatales_Click(object sender, EventArgs e)
+        {
+            var clr1 = Color.Magenta;
+            var clr2 = Color.White;
+
+            for (var n = 0; n < gridSize; n++)
+            {
+                for (var m = 0; m < gridSize; m++)
+                {
+                    // create new Panel control which will be one 
+                    // chess board tile
+                    var newPanel = new Panel
+                    {
+                        Size = new Size(tileSize, tileSize),
+                        Location = new Point(tileSize * n + 450, tileSize * m + 50)
+                    };
+
+                    // add to Form's Controls so that they show up
+                    Controls.Add(newPanel);
+
+
+
+                    // add to our 2d array of panels for future use
+                    _chessBoardPanels[n, m] = newPanel;
+                    // color the backgrounds
+                    if (n % 2 != 0)
+                        newPanel.BackColor = m % 2 != 0 ? clr1 : clr2;
+                    else
+                        newPanel.BackColor = m % 2 != 0 ? clr2 : clr1;
+                }
+            }
+            string path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"Assets\White_Rook.png");
+            _chessBoardPanels[0, 0].BackgroundImage = Image.FromFile(path);
+            _chessBoardPanels[0, 0].BackgroundImageLayout = ImageLayout.Stretch;
+            _chessBoardPanels[7, 7].BackgroundImage = Image.FromFile(path);
+            _chessBoardPanels[7, 7].BackgroundImageLayout = ImageLayout.Stretch;
+        }
     }
 }
