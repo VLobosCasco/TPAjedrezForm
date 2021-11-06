5ea3723e1bf0a770f2f76d45747c6c5d33ce68f4
Lucio Rodriguez <luciorodriguez10@yahoo.com.ar>
2021-11-06 17:49:28 -0300

    Agregado botón que muestra la complejidad

--- Chess/MainMenuChess.Designer.cs
+++ Chess/MainMenuChess.Designer.cs
@@ -35,6 +35,7 @@ namespace Chess
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.btn_exit = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
@@ -48,9 +49,10 @@ namespace Chess
             this.Titulo.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.Titulo.Font = new System.Drawing.Font("Times New Roman", 22F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Titulo.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.Titulo.Location = new System.Drawing.Point(384, 9);
+            this.Titulo.Location = new System.Drawing.Point(576, 14);
+            this.Titulo.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.Titulo.Name = "Titulo";
-            this.Titulo.Size = new System.Drawing.Size(288, 72);
+            this.Titulo.Size = new System.Drawing.Size(428, 104);
             this.Titulo.TabIndex = 0;
             this.Titulo.Text = "Problema del tablero \r\n         de Ajedrez";
             this.Titulo.Click += new System.EventHandler(this.Titulo_Click);
@@ -61,9 +63,10 @@ namespace Chess
             this.startbtn.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.startbtn.Font = new System.Drawing.Font("Times New Roman", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.startbtn.ForeColor = System.Drawing.SystemColors.ControlLight;
-            this.startbtn.Location = new System.Drawing.Point(23, 191);
+            this.startbtn.Location = new System.Drawing.Point(34, 294);
+            this.startbtn.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.startbtn.Name = "startbtn";
-            this.startbtn.Size = new System.Drawing.Size(93, 31);
+            this.startbtn.Size = new System.Drawing.Size(140, 48);
             this.startbtn.TabIndex = 1;
             this.startbtn.Text = "START";
             this.startbtn.UseVisualStyleBackColor = false;
@@ -95,10 +98,9 @@ namespace Chess
             "40",
             "45",
             "50"});
-            this.comboBox1.Location = new System.Drawing.Point(29, 277);
-            this.comboBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.comboBox1.Location = new System.Drawing.Point(44, 426);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(81, 21);
+            this.comboBox1.Size = new System.Drawing.Size(120, 28);
             this.comboBox1.TabIndex = 2;
             this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
@@ -109,9 +111,10 @@ namespace Chess
             this.btn_exit.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.btn_exit.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.btn_exit.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_exit.Location = new System.Drawing.Point(649, 435);
+            this.btn_exit.Location = new System.Drawing.Point(974, 669);
+            this.btn_exit.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btn_exit.Name = "btn_exit";
-            this.btn_exit.Size = new System.Drawing.Size(97, 33);
+            this.btn_exit.Size = new System.Drawing.Size(146, 51);
             this.btn_exit.TabIndex = 3;
             this.btn_exit.UseVisualStyleBackColor = false;
             this.btn_exit.Click += new System.EventHandler(this.btn_exit_Click);
@@ -120,24 +123,43 @@ namespace Chess
             // 
             this.panel1.BackColor = System.Drawing.SystemColors.WindowText;
             this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.button1);
             this.panel1.Controls.Add(this.label3);
             this.panel1.Controls.Add(this.label2);
             this.panel1.Controls.Add(this.label1);
             this.panel1.Controls.Add(this.startbtn);
             this.panel1.Controls.Add(this.comboBox1);
-            this.panel1.Location = new System.Drawing.Point(12, 0);
+            this.panel1.Location = new System.Drawing.Point(18, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(143, 481);
+            this.panel1.Size = new System.Drawing.Size(214, 739);
             this.panel1.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Location = new System.Drawing.Point(7, 219);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(199, 51);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "COMPLEJIDAD";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Georgia", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label3.Location = new System.Drawing.Point(3, 456);
+            this.label3.Location = new System.Drawing.Point(4, 702);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(134, 14);
+            this.label3.Size = new System.Drawing.Size(179, 20);
             this.label3.TabIndex = 5;
             this.label3.Text = "Valentina Lobos Casco\r\n";
             // 
@@ -146,9 +168,10 @@ namespace Chess
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Georgia", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label2.Location = new System.Drawing.Point(2, 8);
+            this.label2.Location = new System.Drawing.Point(3, 12);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(140, 14);
+            this.label2.Size = new System.Drawing.Size(190, 20);
             this.label2.TabIndex = 4;
             this.label2.Text = "Lucio Rodriguez Sapino\r\n";
             // 
@@ -158,26 +181,28 @@ namespace Chess
             this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("Georgia", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.label1.Location = new System.Drawing.Point(15, 250);
+            this.label1.Location = new System.Drawing.Point(22, 385);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(108, 14);
+            this.label1.Size = new System.Drawing.Size(143, 20);
             this.label1.TabIndex = 3;
             this.label1.Text = "Tableros a buscar";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // MainMenuChess
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlLightLight;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(758, 480);
+            this.ClientSize = new System.Drawing.Size(1137, 738);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.btn_exit);
             this.Controls.Add(this.Titulo);
             this.DoubleBuffered = true;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "MainMenuChess";
             this.Text = "MainMenuChess";
             this.Load += new System.EventHandler(this.MainMenuChess_Load);
@@ -198,5 +223,6 @@ namespace Chess
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
     }
 }
--- Chess/MainMenuChess.cs
+++ Chess/MainMenuChess.cs
@@ -55,5 +55,10 @@ namespace Chess
         {
 
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            _ = MessageBox.Show("Dentro del if else que planteamos, nos quedamos con el else ya que este es el de mayor complejidad. Para el mejor caso, se entrara loop while(true) 1 sola vez, es decir que a la primera pasada encuentre un tablero norepetido. De esta forma la complejidad estaría dada por recorrer todo 1 sola vez. Como cada for tiene parámetros fijos que no dependen de la entrada(Ej: N=8 tamaño del tablero), al igual que el loop while dentro de la función atacarCasillas (este corresponde al caso de la reina, que dentro del switch case es el de mayor complejidad), estos tendrán complejidades constantes que se multiplican por la complejidad interior; ésta también será constante ya que tampoco depende de la cantidad de tableros. También tomamos en cuenta que el usuario decide no filtrar las casillas fatales, por lo que no se llama a ésta función y no aporta complejidad. De esta forma, tenemos que la complejidad terminará siendo una constante que resulte de sumar la complejidad de cada línea y función. \nPor otro lado, para un caso general no sabremos cuantas veces tendremos que recorrer el loop while(true), por lo que a la complejidad constante hallada para el mejor caso, deberíamos multiplicarlo por m, es decir, por el número de veces que se recorra el loop while(true). Ademas deberíamos multiplicar este m por la complejidad constante de la función casillasFatales, ya que el usuario puede decidir filtrar para cada tablero sus casillas fatales, por lo que se agregaría una complejidad de (Constante * m). A su vez, lo mencionado anteriormente se multiplicará por un n(tableros) el cual depende de la entrada.Teniendo en cuenta esto, el mejor caso seria cuando n = 1 y el while encuentre el tablero en la primera vuelta.");
+        }
     }
 }
